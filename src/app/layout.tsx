42e59826d11ae17fe29eba3c16ac23c63e65629f
Daniel Kondov <dkondov@sapsservices.com>
2025-09-30 17:55:12 +0300

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,12 +35,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icon.svg", type: "image/svg+xml" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "16x16 32x32", type: "image/x-icon" }
+      { url: "/favicon.png", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" }
     ],
-    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   manifest: "/manifest.json",
   alternates: undefined,
